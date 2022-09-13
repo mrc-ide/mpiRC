d0a4fea142de72c0ab22cf21552e3a5f4295f0a1
Wes Hinsley <w.hinsley@imperial.ac.uk>
2022-09-13 22:02:42 +0100

    Fix scripts

--- scripts/easy-pi-cluster.bat
+++ scripts/easy-pi-cluster.bat
@@ -1,17 +1,7 @@
 @echo off
-
-rem Map up my home directory
-
-net use Q: \\qdrive.dide.ic.ac.uk\homes\wrh1
-
-rem So I am assuming we have a R package repo setup at Q:/R, and
-rem this package is installed in it.
-
+net use Q: \\qdrive.dide.ic.ac.uk\homes\wrh1 >nul 2>nul
 call setr64_4_2_1.bat
 set R_LIBS=Q:/R
 set R_LIBS_USER=Q:/R
-
-mpiexec -n 4 Rscript -e "mpiRC::do_easy_pi()"
-
-net use Q: /delete
-
+Rscript -e "mpiRC::do_easy_pi()"
+net use Q: /delete >nul 2>nul
